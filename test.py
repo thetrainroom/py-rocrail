465b4edb1bc374b087b00ebf82f9eac3d361afd7
Thierry Gschwind <thierry.gschwind@appear.net>
2024-12-06 00:08:01 +0100

    test files

---
+++ test.py
@@ -0,0 +1,19 @@
+import time
+from pyrocrail.pyrocrail import PyRocrail, Action, Trigger
+from pyrocrail.model import Model
+
+def test_script(m: Model):
+    l = list(m._fb_domain.values())
+    fb = l[0]
+    fb.set(not fb.state)
+
+p = PyRocrail()
+#p.add(Action(test_script, Trigger.TIME))
+try:
+    p.start()
+    time.sleep(10)
+    test_script(p.model)
+
+
+finally:
+    p.stop()
